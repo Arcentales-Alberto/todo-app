65d299221fa9151bd3ccd68a91046f05a61ec77b
Alberto-Arcentales <alberto.a@embluemail.com>
2024-11-06 10:15:56 -0300

    Delete .env

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,5 +1,5 @@
 import { config } from 'dotenv'
 config()
 
-export const MONGO_URL = process.env.MONGO_URL || ''; 
-export const SERVER_PORT = process.env.SERVER_PORT || ''; 
+export const MONGO_URL = process.env.MONGO_URL || 'mongodb://db:27017/trpcdb?directConnection=true'; 
+export const SERVER_PORT = process.env.SERVER_PORT || 3000; 
